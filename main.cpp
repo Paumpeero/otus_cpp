c7dc20102007642cf1fe0fb5d04a2d9a1454898d
Антон <a.sysoev@b2b-center.ru>
2024-06-21 05:16:46 +0300

    Bootstrap

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
 #include "src/sdk.h"
 //
 #include <boost/asio/signal_set.hpp>
+#include <boost/log/trivial.hpp>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
 
+using namespace std;
+
 int main()
 {
+  BOOST_LOG_TRIVIAL(error) << "GET" << " /api/v1/config" << endl;
+
   return EXIT_SUCCESS;
 }
