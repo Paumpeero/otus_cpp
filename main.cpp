eefaee6018827e9b111624158dd4e400d3ed881e
Антон <a.sysoev@b2b-center.ru>
2024-06-22 14:04:33 +0300

    solution

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include "src/sdk.h"
 //
 #include <boost/asio/signal_set.hpp>
+#include <boost/log/trivial.hpp>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
 
+using namespace std;
+
 int main()
 {
+  BOOST_LOG_TRIVIAL(info) << "App started..." << endl;
   return EXIT_SUCCESS;
 }
