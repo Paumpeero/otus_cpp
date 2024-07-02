34c21ab0c3b99017d89008dee84d7963c0c5c00c
Антон <a.sysoev@b2b-center.ru>
2024-07-02 14:59:01 +0300

    ip address printer

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "src/sdk.h"
-//
+
 #include <boost/asio/signal_set.hpp>
 #include <iostream>
 #include <mutex>
 #include <thread>
 #include <vector>
 
+#include "src/IPParser.h"
+
+using namespace std;
+using namespace otus_cpp;
+
 int main()
 {
+  cout << GetAsIP( int8_t{-1} ) << endl;  // 255
+  cout << GetAsIP( int16_t{0} ) << endl;  // 0.0
+  cout << GetAsIP( int32_t{2130706433} ) << endl;  // 127.0.0.1
+  cout << GetAsIP( int64_t{8875824491850138409} ) << endl; // 123.45.67.89.101.112.131.41
+  cout << GetAsIP( std::string{"Hello, World!"} ) << endl; // Hello, World!
+  cout << GetAsIP( std::vector<int>{100, 200, 300, 400} ) << endl; // 100.200.300.400
+  cout << GetAsIP( std::list<short>{400, 300, 200, 100} ) << endl; // 400.300.200.100
+//  GetAsIP( std::make_tuple(123, 456, 789, 0) ); // 123.456.789.0
   return EXIT_SUCCESS;
 }
---
+++ src/IPParser.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <string>
+
+namespace otus_cpp
+{
+using namespace std;
+/**
+ * @brief Базовый шаблон для парсинга IP адреса
+ * @tparam T Тип источника данных.
+ * @param source Источник данных
+ * @return Строку, как разобранный IP адрес
+ */
+template<class T>
+string GetAsIP(T source)
+{
+  return to_string(uint8_t(source));
+}
+
+template<>
+string GetAsIP<int32_t>(int32_t source)
+{
+  uint32_t source_as_uint = source;
+  uint8_t* source_as_vector = reinterpret_cast<uint8_t*>(&source_as_uint);
+  string ret;
+
+  for (uint64_t i = 3; i != uint64_t(-1); --i)
+  {
+    if (i != 3)
+    {
+      ret += "."s;
+    }
+    ret += to_string(source_as_vector[i]);
+  }
+
+  return ret;
+}
+
+template<>
+string GetAsIP<string>(string source) { return source; }
+
+template<>
+string GetAsIP<vector<int>>(vector<int> source)
+{
+  string ret;
+
+  for (int32_t i = 0; i != source.size(); ++i)
+  {
+    if (i) ret += "."s;
+
+    ret += to_string(source[i]);
+  }
+
+  return ret;
+}
+
+template<>
+string GetAsIP<list<short>>(list<short> source)
+{
+  string ret;
+
+  for (auto iter = source.begin(), end = source.end(); iter != end; ++iter)
+  {
+    if (iter != source.begin()) ret += "."s;
+
+    ret += to_string(*iter);
+  }
+
+  return ret;
+}
+}
