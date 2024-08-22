9bf795e4280ede146feb5820e72a3cde25f13dd0
Антон <a.sysoev@b2b-center.ru>
2024-08-22 20:51:31 +0300

    Базовая структура

--- src/matrix.h
+++ src/matrix.h
@@ -5,4 +5,28 @@ namespace lib
 {
 using namespace std::string_literals;
 
+template<class T, T Default, size_t DimensionsCount, class Dimension, class... Dimensions>
+class IMatrix;
+
+template<class T, T Default, size_t DimensionsCount, class Dimension = size_t, class... Dimensions>
+class IMatrixIter
+{
+ protected:
+  using Matrix = IMatrix<T, Default, DimensionsCount, Dimension, Dimensions...>;
+ public:
+  virtual Matrix* operator ->() = 0;
+  virtual ~IMatrixIter() = default;
+};
+
+template<class T, T Default, size_t DimensionsCount, class Dimension = size_t, class... Dimensions>
+class IMatrix
+{
+ protected:
+  using Cell = IMatrix<T, Default, DimensionsCount - 1, Dimensions...>;
+  using Iter = IMatrixIter<T, Default, DimensionsCount, Dimension, Dimensions...>;
+ public:
+  virtual Iter begin() = 0;
+  virtual Iter end() = 0;
+  virtual ~IMatrix() = default;
+};
 }
