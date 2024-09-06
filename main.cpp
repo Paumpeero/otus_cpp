79125db56499e5526aaaea879f81929d3de1a009
Антон <a.sysoev@b2b-center.ru>
2024-09-06 17:14:24 +0300

    Набросок интерфейсов

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <thread>
 #include <vector>
+#include <fstream>
 
 #include "src/Scanner.h"
 #include "src/Timer.h"
@@ -21,7 +22,18 @@ int main(int argc, char** argv)
   unique_ptr<ITimer> timer = make_unique<Timer>();
   unique_ptr<IParser> parser = make_unique<Parser>();
   unique_ptr<ILogger> logger = make_unique<Logger>();
+  unique_ptr<ILogger> printer = make_unique<Logger>();
   unique_ptr<IController> controller = make_unique<Controller>();
+
+  auto file_output_stream = ofstream("./bulk_"s + to_string(timer->GetTime()));
+
+  logger->SetOutputStream(cout);
+  printer->SetOutputStream(file_output_stream);
+
+  logger->Log("Qwerty"s);
+  printer->Log("Qwerty"s);
+
+  file_output_stream.close();
 
   return EXIT_SUCCESS;
 }
